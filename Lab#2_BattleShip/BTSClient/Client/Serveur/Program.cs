d7c000a4dfd4439b1b29409fe6471b74cf03c8a1
C-John-117 <2310197@etu.cchic.ca>
2025-09-03 15:43:19 -0400

    Premier depot Git Ici ce qui est fonctionelle c'est l'affichage de tableau . Le but est que chaque prenne conaissance
    de ce qui est deja fait pour que on puisse avancer.

---
+++ Lab#2_BattleShip/BTSClient/Client/Serveur/Program.cs
@@ -0,0 +1,16 @@
+﻿using BattleShip;
+namespace Serveur
+  
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GrilleBattle Grille = new GrilleBattle('x');
+
+            Grille.Tir("D2", 'X');
+            Grille.AfficherGrille();
+
+        }
+    }
+}
